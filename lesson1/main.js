852e3fde154dc81f60cc535593467e1a477f9253
GrayHead <ss.zhuravlov@gmail.com>
2022-01-31 21:39:43 +0200

    lesson 1

---
+++ lesson1/main.js
@@ -0,0 +1,130 @@
+// let xxx = 100500;
+// console.log(xxx + 200);
+// console.log(xxx + 300);
+// console.log(typeof xxx);
+// let someMsg = `100500`;
+// console.log(someMsg);
+// console.log(typeof someMsg);
+// let PI = 3.14;
+// PI = 1231232;
+// console.log(PI);
+
+//
+// {
+//     var a = 'hello';
+//     let b = 'world';
+// }
+// console.log(a);
+// console.log(b);
+//
+
+// let yyy = 100500;
+// console.log(yyy);
+// yyy = 'asdagsfd';
+// console.log(yyy);
+
+
+// console.log(true);
+// console.log(false);
+// // > < >= <= == === != !==  true false
+// let bool1 = 5 > 6
+// console.log(bool1);
+// console.log(typeof true);
+// let bool2 = 5 === '5';
+// console.log(typeof bool2);
+
+// let num = 100500;
+// //              0    1    2    3
+// let foo = 100;
+// let numbers = [foo, 200, 300, num, true, 'ashdahgsdf'];
+// console.log(numbers);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+// console.log(numbers.length);
+
+
+// //          0    1    2        3
+// //                         0   1    2
+// let arr = ['a', 'b', 'c', [11, 22, 33]];
+// console.log(arr[3][2]);
+
+
+// let product1 = {
+//     title: 'milk',
+//     price: 23,
+//     quantity: 2
+// }
+// console.log(product1);
+// console.log(product1.title);
+// console.log(product1.quantity);
+//
+// let product2 = {
+//     title: 'beer',
+//     price: 40,
+//     quantity: 3,
+//     manufacturer: {
+//         title: 'chernihivske',
+//         city: 'Chernihiv',
+//         zip: 43055,
+//         conservants: [
+//             {id: 'c155', status: 1},
+//             {id: 'a10', status: 2},
+//             {id: 'b1', status: 1}
+//         ]
+//     },
+//
+// }
+// console.log(product2.title)
+// console.log(product2.manufacturer.title);
+// console.log(product2['title']);
+// console.log(product2['manufacturer']['title']);
+// console.log(product2['manufacturer'].title);
+// console.log(product2.manufacturer.conservants[2]);
+// console.log(product2.manufacturer.conservants[1].status);
+
+
+// console.log(5 + 10); // 15
+// console.log(5 - 10);
+// console.log(5 * 10);
+// console.log(5 / 10);
+// console.log(10 % 3);// 1
+
+console.log('10' + '5' + 'ashdfashdgf' + ' ' + '!!!');
+console.log(10 - '2');
+// console.log(typeof NaN);
+console.log('qweqasd' / []);
+let x = null //->{name :'vasya'}
+
+// let foo = 100500 ; // number
+// let foo = 100500 +''; // string
+// console.log(foo);
+
+// let foo = '200600.123';
+// // console.log(+foo);
+// console.log(parseInt(foo));
+// console.log(parseFloat(foo));
+
+// console.log(!false);
+// console.log(!!' ');
+
+console.log(!!0);
+console.log(!!'');
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!null);
+
+let msg = 'hello okten';
+document.write('<div>' + msg + '</div>');
+document.write(`<div>${msg}</div>`);
+
+
+
+
+
+
+
+
+
+
+
+
